47a6e969fc05122171479e765828e4dfa00ed136
Mozl <louis.moselhi@bbc.co.uk>
2019-10-03 11:03:55 +0100

    add connect to CoursesPage, show courses in UI, add prettier and eslint

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,30 +1,52 @@
-import React from "react";
+import React from 'react';
+import { connect } from 'react-redux';
+import * as courseActions from '../../redux/actions/courseActions';
+import PropTypes from 'prop-types';
 
 class CoursesPage extends React.Component {
   state = {
     course: {
-      title: ""
+      title: ''
     }
-  }
+  };
 
   handleChange = e => {
-    const course = {...this.state.course, title: e.target.value};
+    const course = { ...this.state.course, title: e.target.value };
     this.setState({ course });
-  }
+  };
 
   handleSubmit = e => {
     e.preventDefault();
-  }
+    this.props.dispatch(courseActions.createCourse(this.state.course));
+  };
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <h2>Courses</h2>
         <h2>Add Course</h2>
-        <input type="text" onChange={this.handleChange} value={this.state.course.title}></input>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        ></input>
         <input type="submit" value="Save" />
+        {this.props.courses.map(course => (
+          <div key={course.title}>{course.title}</div>
+        ))}
       </form>
-    )
+    );
   }
 }
 
-export default CoursesPage;
+CoursesPage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  dispatch: PropTypes.func.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses
+  };
+}
+
+export default connect(mapStateToProps)(CoursesPage);
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,13 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 
 export default function configureStore(initialState) {
-  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
   return createStore(
-    rootReducer, initialState, composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
+    rootReducer,
+    initialState,
+    composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
   );
 }
