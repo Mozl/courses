d38f0f7eef32a8310ee0819b4048002e8af6e6eb
Mozl <louis.moselhi@bbc.co.uk>
2019-10-04 11:20:12 +0100

    adding load success async dispatch

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -5,36 +5,14 @@ import { createCourseAction } from '../../redux/actions/courseActions';
 import PropTypes from 'prop-types';
 
 class CoursesPage extends React.Component {
-  state = {
-    course: {
-      title: ''
-    }
-  };
-
-  handleChange = e => {
-    const course = { ...this.state.course, title: e.target.value };
-    this.setState({ course });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.createCourse(this.state.course);
-  };
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
+      <>
         <h2>Courses</h2>
-        <h2>Add Course</h2>
-        <input
-          type="text"
-          onChange={this.handleChange}
-          value={this.state.course.title}
-        ></input>
-        <input type="submit" value="Save" />
         {this.props.courses.map(course => (
           <div key={course.title}>{course.title}</div>
         ))}
-      </form>
+      </>
     );
   }
 }
--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,8 +1,28 @@
-import { CREATE_COURSE } from '../actions/actionTypes';
+import { CREATE_COURSE, LOAD_COURSES_SUCCESS } from '../actions/actionTypes';
+import * as CourseApi from '../../api/courseApi';
 
 export function createCourseAction(course) {
   return {
     type: CREATE_COURSE,
     course
+  };
+}
+
+export function loadCoursesSuccess(courses) {
+  return {
+    type: LOAD_COURSES_SUCCESS,
+    courses
+  };
+}
+
+export function loadCourses() {
+  return function(dispatch) {
+    return CourseApi.getCourses()
+      .then(courses => {
+        dispatch(loadCoursesSuccess(courses));
+      })
+      .catch(error => {
+        throw error;
+      });
   };
 }
